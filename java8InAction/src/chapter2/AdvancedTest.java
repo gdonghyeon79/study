e94c7d2b1257b9359729ce7a76f96939dca86e4c
godonghyeon <gdonghyeon79@gmail.com>
2021-05-23 22:06:17 +0900

    java와 junit 활용한 실용주의 단위 테스트 첫 예제

--- java8InAction/src/chapter2/AdvancedTest.java
+++ java8InAction/src/chapter2/AdvancedTest.java
@@ -4,7 +4,7 @@ import chapter2.vo.Apple;
 
 public class AdvancedTest {
     public static void main(String[] args) {
-
+        System.out.println("test");
     }
 
     // predicate : boolean을 반환하는 함수
